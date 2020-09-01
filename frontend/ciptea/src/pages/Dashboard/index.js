af07531373a9fd8c4c4d598a0dc641253c32ec94
Lindson França <l1nds0n@hotmail.com>
2020-09-01 17:32:31 -0300

    Navegação para a carteira adicionada desde o Dashboard

--- frontend/ciptea/src/pages/Dashboard/index.js
+++ frontend/ciptea/src/pages/Dashboard/index.js
@@ -24,7 +24,9 @@ export default function Dashboard(){
                    <li key={registro.id}>
                         {/* ver configuração de upload de imagens */}
                        {/* <header style={{ backgroundImage: 'url(https://blog.influx.com.br/storage/app/uploads/public/67d/18f/2fd/67d18f2fdf601e40e21e8dc4e70247ca62c6ac83.jpg)'}}></header> */}
+                       <Link to={`/card/${registro.id}`}>
                        <header style={{ backgroundImage: `url(http://localhost:3333/files/${registro.fotoRostoPath})`}}></header>
+                       </Link>
                        <strong>{registro.nomeTitular}</strong>
                         <span>{registro.cpfTitular ? `${registro.cpfTitular}` : `${registro.rgTitular}`}</span>
                    </li>

c7dcfdea9e63645fcdd44c331f8514643cff1e15
Lindson França <l1nds0n@hotmail.com>
2020-09-20 01:41:22 -0300

    Animação no logo

--- frontend/src/components/MainHeader/index.js
+++ frontend/src/components/MainHeader/index.js
@@ -3,24 +3,25 @@ import React from 'react';
 import logo from '../../assets/logo.svg'
 import './styles.css';
 
-export default function MainHeader(){
+export default function MainHeader() {
 
-    return(
+    return (
         <>
-        <div id="MainHeader">
-            <div id="MHContent">
-                <a href="/">
-                    <img src={ logo }
-                        id="logo"
-                        alt="CipteaLogo"/>
-                </a>        
-                    
-                <h1 className="title">CIPTEA</h1>
-                <p className="subtitle anim-typewriter">
-                Carteira de Identificação da Pessoa com Espectro Autista
+            <div id="MainHeader">
+                <div id="MHContent">
+                    <a href="/">
+                        <img
+                            src={logo}
+                            id="logo"
+                            alt="CipteaLogo" />
+                    </a>
+
+                    <h1 className="title">CIPTEA</h1>
+                    <p className="subtitle anim-typewriter">
+                        Carteira de Identificação da Pessoa com Espectro Autista
                 </p>
+                </div>
             </div>
-        </div>
         </>
     )
 }

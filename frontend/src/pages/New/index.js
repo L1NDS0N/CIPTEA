5fe0daee125e0f7bd8be5e7811bde8231e5eb654
Lindson França <l1nds0n@hotmail.com>
2020-09-24 13:10:10 -0300

    Adicionando o restante dos anexos necessários

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useMemo } from 'react';
 import MaskedInput from 'react-text-mask';
-import { FiCamera } from 'react-icons/fi';
+import { FiCamera, FiPaperclip } from 'react-icons/fi';
 
 import NavigationButtons from '../../components/NavigationButtons';
 import api from '../../services/api';
@@ -17,10 +17,16 @@ export default function New({ history }) {
     const [emailContato, setEmailContato] = useState('');
     const [numeroContato, setNumeroContato] = useState('');
     const [fotoRostoPath, setFotoRostoPath] = useState(null);
+    const [laudoMedicoPath, setLaudoMedicoPath] = useState(null);
 
-    const preview = useMemo(() => {
+    // Não permitir outros formatos senão imagem
+    const previewFotoRosto = useMemo(() => {
         return fotoRostoPath ? URL.createObjectURL(fotoRostoPath) : null;
     }, [fotoRostoPath])
+
+    const previewLaudoMedico = useMemo(() => {
+        return laudoMedicoPath ? URL.createObjectURL(laudoMedicoPath) : null;
+    }, [laudoMedicoPath])
 
     async function handleSubmit(event) {
         const token = localStorage.getItem('userToken');
@@ -37,6 +43,7 @@ export default function New({ history }) {
         data.append('emailContato', emailContato)
         data.append('numeroContato', numeroContato)
         data.append('fotoRostoPath', fotoRostoPath)
+        data.append('laudoMedicoPath', laudoMedicoPath)
 
         await api.post('/carteiras', data,
             { headers: { 'Authorization': 'Bearer ' + token } }
@@ -45,124 +52,149 @@ export default function New({ history }) {
     }
     return (
         <>
-        <NavigationButtons/>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="nomeResponsavel">Nome do responsável *</label>
-            <input
-                maxLength="250"
-                required
-                id="nomeResponsavel"
-                placeholder="Insira o nome completo do responsável/tutor"
-                value={nomeResponsavel}
-                onChange={event => setNomeResponsavel(event.target.value)}
-            />
-
-            <label htmlFor="cpfResponsavel">CPF do responsável *</label>
-            <MaskedInput
-                mask={[/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/]}
-                required
-                guide={false}
-                id="cpfResponsavel"
-                placeholder="Insira o CPF do responsável/tutor"
-                value={cpfResponsavel}
-                onChange={event => setCpfResponsavel(event.target.value)}
-            />
-
-            <label htmlFor="rgResponsavel">RG do responsável *</label>
-            <input
-                maxLength="20"
-                required
-                id="rgResponsavel"
-                placeholder="Insira o RG do responsável/tutor"
-                value={rgResponsavel}
-                onChange={event => setRgResponsavel(event.target.value)}
-            />
-
-            <label htmlFor="nomeTitular">Nome do titular *</label>
-            <input
-                maxLength="250"
-                required
-                id="nomeTitular"
-                placeholder="Insira o nome completo do titular"
-                value={nomeTitular}
-                onChange={event => setNomeTitular(event.target.value)}
-            />
-
-            <label htmlFor="cpfTitular">CPF do titular *</label>
-            <MaskedInput
-                mask={[/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/]}
-                required
-                guide={false}
-                id="cpfTitular"
-                placeholder="Insira o CPF do titular"
-                value={cpfTitular}
-                onChange={event => setCpfTitular(event.target.value)}
-            />
-
-            <label htmlFor="rgTitular">RG do titular *</label>
-            <input
-                maxLength="20"
-                required
-                id="rgTitular"
-                placeholder="Insira o RG do titular"
-                value={rgTitular}
-                onChange={event => setRgTitular(event.target.value)}
-            />
-
-            <label htmlFor="dataNascimento">Data de nascimento *</label>
-            <input
-                type="date"
-                required
-                id="dataNascimento"
-                placeholder="Insira a Data de Nascimento do Titular"
-                value={dataNascimento}
-                onChange={event => setDataNascimento(event.target.value)}
-            />
-
-            <label htmlFor="emailContato">Email</label>
-            <input
-                type="emailContato"
-                id="emailContato"
-                placeholder="Insira um e-mail para contato"
-                value={emailContato}
-                onChange={event => setEmailContato(event.target.value)}
-            />
-
-            <label htmlFor="numeroContato">Telefone</label>
-            <input
-                type="numeroContato"
-                id="numeroContato"
-                placeholder="Insira um número de telefone para contato"
-                value={numeroContato}
-                onChange={event => setNumeroContato(event.target.value)}
-            />
-
-            <label
-                htmlFor="previewFoto3x4"
-                id="foto3x4"
-                style={{ backgroundImage: `url(${preview})` }}
-                className={fotoRostoPath ? 'has-fotoRosto' : ''}
-            >
-                <span>Selecione a foto 3x4</span>
+            <NavigationButtons />
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="nomeResponsavel">Nome do responsável *</label>
                 <input
-                    id="previewFoto3x4"
-                    type="file"
-                    onChange={event => setFotoRostoPath(event.target.files[0])}
+                    maxLength="250"
+                    required
+                    id="nomeResponsavel"
+                    placeholder="Insira o nome completo do responsável/tutor"
+                    value={nomeResponsavel}
+                    onChange={event => setNomeResponsavel(event.target.value)}
                 />
-                <div>
-                    <FiCamera size={30} alt="Selecione a foto 3x4" />
+
+                <label htmlFor="cpfResponsavel">CPF do responsável *</label>
+                <MaskedInput
+                    mask={[/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/]}
+                    required
+                    guide={false}
+                    id="cpfResponsavel"
+                    placeholder="Insira o CPF do responsável/tutor"
+                    value={cpfResponsavel}
+                    onChange={event => setCpfResponsavel(event.target.value)}
+                />
+
+                <label htmlFor="rgResponsavel">RG do responsável *</label>
+                <input
+                    maxLength="20"
+                    required
+                    id="rgResponsavel"
+                    placeholder="Insira o RG do responsável/tutor"
+                    value={rgResponsavel}
+                    onChange={event => setRgResponsavel(event.target.value)}
+                />
+
+                <label htmlFor="nomeTitular">Nome do titular *</label>
+                <input
+                    maxLength="250"
+                    required
+                    id="nomeTitular"
+                    placeholder="Insira o nome completo do titular"
+                    value={nomeTitular}
+                    onChange={event => setNomeTitular(event.target.value)}
+                />
+
+                <label htmlFor="cpfTitular">CPF do titular *</label>
+                <MaskedInput
+                    mask={[/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/]}
+                    required
+                    guide={false}
+                    id="cpfTitular"
+                    placeholder="Insira o CPF do titular"
+                    value={cpfTitular}
+                    onChange={event => setCpfTitular(event.target.value)}
+                />
+
+                <label htmlFor="rgTitular">RG do titular *</label>
+                <input
+                    maxLength="20"
+                    required
+                    id="rgTitular"
+                    placeholder="Insira o RG do titular"
+                    value={rgTitular}
+                    onChange={event => setRgTitular(event.target.value)}
+                />
+
+                <label htmlFor="dataNascimento">Data de nascimento *</label>
+                <input
+                    type="date"
+                    required
+                    id="dataNascimento"
+                    placeholder="Insira a Data de Nascimento do Titular"
+                    value={dataNascimento}
+                    onChange={event => setDataNascimento(event.target.value)}
+                />
+
+                <label htmlFor="emailContato">Email</label>
+                <input
+                    type="emailContato"
+                    id="emailContato"
+                    placeholder="Insira um e-mail para contato"
+                    value={emailContato}
+                    onChange={event => setEmailContato(event.target.value)}
+                />
+
+                <label htmlFor="numeroContato">Telefone</label>
+                <input
+                    type="numeroContato"
+                    id="numeroContato"
+                    placeholder="Insira um número de telefone para contato"
+                    value={numeroContato}
+                    onChange={event => setNumeroContato(event.target.value)}
+                />
+
+                <div className="anexosCadastro">
+                    <label
+                        htmlFor="previewFoto3x4"
+                        id="foto3x4"
+                        style={{ backgroundImage: `url(${previewFotoRosto})` }}
+                        className={fotoRostoPath ? 'has-fotoRosto' : ''}
+                    >
+                        <span>Selecione a foto 3x4</span>
+                        <input
+                            id="previewFoto3x4"
+                            type="file"
+                            onChange={event => setFotoRostoPath(event.target.files[0])}
+                        />
+                        <div>
+                            <FiCamera size={30} alt="Selecione a foto 3x4" />
+                        </div>
+                    </label>
+
+                    <label
+                        htmlFor="previewLaudoMedico"
+                        id="laudoMedico"
+                        // style={{ backgroundImage: `url(${previewLaudoMedico})` }}
+                        className={laudoMedicoPath ? 'has-laudoMedico' : ''}
+                    >
+                        <span>Selecione o laudo médico</span>
+                        <input
+                            id="previewLaudoMedico"
+                            type="file"
+                            onChange={event => setLaudoMedicoPath(event.target.files[0])}
+                        />
+                        <div>
+                            <FiPaperclip size={30} alt="Selecione o laudo médico" />
+                        </div>
+                        {laudoMedicoPath ?
+                            <a
+                                className="pathLink"
+                                href={previewLaudoMedico}
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                <p>
+                                    <p className="nomeDoArquivo">{laudoMedicoPath.name}</p>
+                                    foi selecionado, clique para visualizar em nova guia
+                                </p>
+                            </a>
+                            : ''}
+                    </label>
                 </div>
-            </label>
 
-            {/* <MaskedInput
-                mask={[/[1-9]/, /\d/, '.']}
-                placeholder="teste"
-                guide={false}
-                id="cpf"
-            /> */}
 
-            <button className="btn" type="submit">Cadastrar</button>
-        </form>
+                <button className="btn" type="submit">Cadastrar</button>
+            </form>
         </>
     )
 }

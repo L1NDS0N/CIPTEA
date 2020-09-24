4d8db3bc74d6821c2f59b654e88d17b4dd7a3364
Lindson França <l1nds0n@hotmail.com>
2020-09-23 21:55:15 -0300

    melhoria no mainfooter

--- frontend/src/components/MainFooter/index.js
+++ frontend/src/components/MainFooter/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
+import { FiGithub, FiCamera } from 'react-icons/fi';
 import './styles.css';
 
-export default function MainFooter(){
-    return(
+export default function MainFooter() {
+    return (
         <>
             <footer>
                 <p className="devCredits" >
-                Developed and distruted by <a 
-                    href="https://github.com/l1nds0n/"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                <strong> L1NDS0N © 2020 </strong>
+                    Developed and distruted by <a
+                        href="https://github.com/l1nds0n/"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        <strong><FiGithub /> L1NDS0N  </strong>
+                    © 2020
                     </a>
-                    </p> 
-                    <p className="imageCredits" >
-                    Fundo por <a 
+                </p>
+                <p className="imageCredits" >
+                    Fundo por <a
                         href="https://wendelljefferson.com.br/"
                         target="_blank"
                         rel="noopener noreferrer" >
-                <strong> Wendell Jefferson  </strong>
-                    </a> 
-                    </p> 
+                        <strong> <FiCamera/> Wendell Jefferson  </strong>
+                    </a>
+                </p>
             </footer>
         </>
     )

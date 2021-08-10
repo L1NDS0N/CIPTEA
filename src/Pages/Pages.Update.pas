955d89597d9e175c8cb939c04dc7f01dec8489af
Lindson França <l1nds0n@hotmail.com>
2021-08-10 17:24:20 -0300

    Página de alteração de cadastro e providers para o dashboard

---
+++ src/Pages/Pages.Update.pas
@@ -0,0 +1,156 @@
+unit Pages.Update;
+
+interface
+
+uses
+  Router4D.Interfaces,
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Graphics,
+  FMX.Dialogs,
+  FMX.Ani,
+  FMX.StdCtrls,
+  FMX.Objects,
+  FMX.DateTimeCtrls,
+  FMX.Controls.Presentation,
+  FMX.Edit,
+  FMX.Layouts,
+  Services.New,
+  Router4D.Props;
+
+type
+  TPageUpdate = class(TForm, iRouter4DComponent)
+    dlgLaudoMedico: TOpenDialog;
+    lytUpdate: TLayout;
+    lytInterno: TLayout;
+    VertScrollBox: TVertScrollBox;
+    edtCpfResponsavel: TEdit;
+    edtCpfTitular: TEdit;
+    edtDataNascimento: TDateEdit;
+    edtEmailContato: TEdit;
+    edtNomeResponsavel: TEdit;
+    edtNomeTitular: TEdit;
+    edtNumeroContato: TEdit;
+    edtRgResponsavel: TEdit;
+    edtRgTitular: TEdit;
+    lblcpfResponsavel: TLabel;
+    lblcpfTitular: TLabel;
+    lbldataNascimento: TLabel;
+    lblemailContato: TLabel;
+    lblnomeResponsavel: TLabel;
+    lblnomeTitular: TLabel;
+    lblnumeroContato: TLabel;
+    lblRgResponsavel: TLabel;
+    lblrgTitular: TLabel;
+    lytAnexos: TLayout;
+    rctFotoRosto: TRectangle;
+    rctLaudoMedico: TRectangle;
+    btnSalvar: TButton;
+    lytHeader: TLayout;
+    btnVoltar: TSpeedButton;
+    FloatAnimation: TFloatAnimation;
+    dlgFotoRosto: TOpenDialog;
+    lblID: TLabel;
+    procedure FormCreate(Sender: TObject);
+    procedure btnVoltarClick(Sender: TObject);
+    procedure btnSalvarClick(Sender: TObject);
+    procedure lytInternoClick(Sender: TObject);
+    private
+      serviceNew: TServiceNew;
+    public
+      procedure updateFields(id: string);
+      function Render: TFmxObject;
+      procedure UnRender;
+      [Subscribe]
+      procedure Props(aValue: TProps);
+  end;
+
+var
+  PageUpdate: TPageUpdate;
+
+implementation
+
+uses
+  Router4D,
+  Pages.Dashboard;
+{$R *.fmx}
+{ TPageUpdate }
+
+procedure TPageUpdate.btnSalvarClick(Sender: TObject);
+begin
+  if (serviceNew.mtCadastroCarteiraPTEAid.AsInteger > 0) then
+    serviceNew.mtCadastroCarteiraPTEA.Edit
+  else
+    serviceNew.mtCadastroCarteiraPTEA.Append;
+  serviceNew.mtCadastroCarteiraPTEANomeResponsavel.AsString := edtNomeResponsavel.Text;
+  serviceNew.mtCadastroCarteiraPTEADataNascimento.AsDateTime := edtDataNascimento.Date;
+  serviceNew.mtCadastroCarteiraPTEACpfResponsavel.AsString := edtCpfResponsavel.Text;
+  serviceNew.mtCadastroCarteiraPTEANumeroContato.AsString := edtNumeroContato.Text;
+  serviceNew.mtCadastroCarteiraPTEARgResponsavel.AsString := edtRgResponsavel.Text;
+  serviceNew.mtCadastroCarteiraPTEAEmailContato.AsString := edtEmailContato.Text;
+  serviceNew.mtCadastroCarteiraPTEANomeTitular.AsString := edtNomeTitular.Text;
+  serviceNew.mtCadastroCarteiraPTEACpfTitular.AsString := edtCpfTitular.Text;
+  serviceNew.mtCadastroCarteiraPTEARgTitular.AsString := edtRgTitular.Text;
+  serviceNew.Salvar;
+
+  TRouter4D.Link.&To('Dashboard');
+end;
+
+procedure TPageUpdate.btnVoltarClick(Sender: TObject);
+begin
+  TRouter4D.Link.&To('Dashboard');
+end;
+
+procedure TPageUpdate.FormCreate(Sender: TObject);
+begin
+  serviceNew := TServiceNew.Create(Self);
+end;
+
+procedure TPageUpdate.lytInternoClick(Sender: TObject);
+begin
+  serviceNew.Free;
+end;
+
+procedure TPageUpdate.Props(aValue: TProps);
+begin
+  try
+    if (aValue.PropString <> '') and (aValue.Key = 'IdCarteiraToUpdate') then
+      serviceNew.GetById(aValue.PropString);
+    lblID.Text := '#' + aValue.PropString;
+    edtNomeResponsavel.Text := serviceNew.mtCadastroCarteiraPTEANomeResponsavel.AsString;
+    edtCpfResponsavel.Text := serviceNew.mtCadastroCarteiraPTEACpfResponsavel.AsString;
+    edtCpfTitular.Text := serviceNew.mtCadastroCarteiraPTEACpfTitular.AsString;
+    edtDataNascimento.Date := serviceNew.mtCadastroCarteiraPTEADataNascimento.AsDateTime;
+    edtEmailContato.Text := serviceNew.mtCadastroCarteiraPTEAEmailContato.AsString;
+    edtNomeTitular.Text := serviceNew.mtCadastroCarteiraPTEANomeTitular.AsString;
+    edtNumeroContato.Text := serviceNew.mtCadastroCarteiraPTEANumeroContato.AsString;
+    edtRgResponsavel.Text := serviceNew.mtCadastroCarteiraPTEARgResponsavel.AsString;
+    edtRgTitular.Text := serviceNew.mtCadastroCarteiraPTEARgTitular.AsString;
+  finally
+    aValue.Free;
+  end;
+
+end;
+
+function TPageUpdate.Render: TFmxObject;
+begin
+  Result := lytUpdate;
+end;
+
+procedure TPageUpdate.UnRender;
+begin
+  //
+end;
+
+procedure TPageUpdate.updateFields(id: string);
+begin
+  lblID.Text := id;
+end;
+
+end.
---
+++ src/Pages/Providers/Frames.DashboardDetail.pas
@@ -0,0 +1,79 @@
+unit Frames.DashboardDetail;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Graphics,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Dialogs,
+  FMX.StdCtrls,
+  FMX.Ani,
+  FMX.Objects,
+  FMX.Controls.Presentation,
+  Providers.Types;
+
+type
+  TFrameDashboardDetail = class(TFrame)
+    retContent: TRectangle;
+    lblCPFTitular: TLabel;
+    lblNomeTitular: TLabel;
+    crlDelete: TCircle;
+    imgDelete: TPath;
+    lineSeparator: TLine;
+    crlEdit: TCircle;
+    imgEdit: TPath;
+    FloatAnimation1: TFloatAnimation;
+    retTop: TRectangle;
+    Imagem: TImage;
+    Rectangle1: TRectangle;
+    lblID: TLabel;
+    retRight: TRectangle;
+    procedure crlEditClick(Sender: TObject);
+    procedure retContentClick(Sender: TObject);
+    procedure crlDeleteClick(Sender: TObject);
+    private
+      FId: string;
+      FOnDelete: TEventCallBack;
+      FOnUpdate: TEventCallBack;
+    public
+      property Id: string read FId write FId;
+      property OnDelete: TEventCallBack read FOnDelete write FOnDelete;
+      property OnUpdate: TEventCallBack read FOnUpdate write FOnUpdate;
+  end;
+
+implementation
+
+{$R *.fmx}
+
+procedure TFrameDashboardDetail.crlDeleteClick(Sender: TObject);
+begin
+  {$IFDEF MSWINDOWS}
+  if Assigned(FOnDelete) then
+    FOnDelete(Self, FId);
+  {$ENDIF}
+end;
+
+procedure TFrameDashboardDetail.crlEditClick(Sender: TObject);
+begin
+  {$IFDEF MSWINDOWS}
+  if Assigned(FOnUpdate) then
+    FOnUpdate(Self, FId);
+  {$ENDIF}
+end;
+
+procedure TFrameDashboardDetail.retContentClick(Sender: TObject);
+begin
+  {$IFDEF MSWINDOWS}
+  if Assigned(FOnUpdate) then
+    FOnUpdate(Self, FId);
+  {$ENDIF}
+end;
+
+end.

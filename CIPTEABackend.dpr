024dac3a6b7bc199e2c4b862da83c53336834a13
Lindson França <l1nds0n@hotmail.com>
2021-08-11 13:14:34 -0300

    Compressão e Etag adicionados

--- CIPTEABackend.dpr
+++ CIPTEABackend.dpr
@@ -5,16 +5,23 @@ program CIPTEABackend;
 
 uses
   Horse,
+  Horse.ETag,
   Horse.Jhonson,
+  Horse.Compression,
   Horse.HandleException,
   System.SysUtils,
   Controllers.CarteiraPTEA in 'src\Controllers\Controllers.CarteiraPTEA.pas',
-  Services.CarteiraPTEA in 'src\Services\Services.CarteiraPTEA.pas' {ServiceCarteiraPTEA: TDataModule},
+  Services.CarteiraPTEA in 'src\Services\Services.CarteiraPTEA.pas' {ServiceCarteiraPTEA: TDataModule} ,
   Services.Connection in 'src\Services\Services.Connection.pas' {ServiceConnection: TDataModule};
 
 begin
-  THorse.Use(Jhonson).Use(HandleException);
-  Controllers.CarteiraPTEA.Registry;
-  THorse.Listen(9000);
+  THorse
+  .Use(Compression())
+  .Use(Jhonson)
+  .Use(ETag)
+  .Use(HandleException);
 
+  Controllers.CarteiraPTEA.Registry;
+
+  THorse.Listen(9000);
 end.

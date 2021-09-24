5eb09513b6fc3e712332baf2cf3047001a6f4f4e
Lindson França <l1nds0n@hotmail.com>
2021-09-24 15:25:54 -0300

    custom progress bar based on Animation time

--- src/Pages/Pages.Principal.pas
+++ src/Pages/Pages.Principal.pas
@@ -6,20 +6,19 @@ uses
   System.SysUtils,
   System.Types,
   System.UITypes,
-  System.Classes,
   System.Variants,
   FMX.Types,
   FMX.Controls,
   FMX.Forms,
   FMX.Graphics,
-  FMX.Dialogs,
   FMX.Ani,
   FMX.Controls.Presentation,
   FMX.StdCtrls,
   FMX.Objects,
   FMX.Layouts,
   Router4D.History,
-  FMX.Effects;
+  FMX.Effects,
+  System.Classes;
 
 type
   TPagePrincipal = class(TForm)
@@ -29,11 +28,14 @@ type
     rctPrincipal: TRectangle;
     lytMaster: TLayout;
     VertScrollBox: TVertScrollBox;
+    ProgressBar: TProgressBar;
+    lytProgress: TLayout;
     procedure FormCreate(Sender: TObject);
     private
       procedure Router;
     public
       procedure Animation(aLayout: TFMXObject);
+      procedure ProgressBarAnimation(aProgressBar: TFMXObject);
   end;
 
 var
@@ -48,13 +50,29 @@ uses
   Pages.New,
   Pages.Update,
   Pages.Editor,
-  Pages.Print;
+  Pages.Print,
+  FMX.Dialogs;
 
 {$R *.fmx}
 
 procedure TPagePrincipal.FormCreate(Sender: TObject);
 begin
   Self.Router;
+  ProgressBar.Visible := false;
+end;
+
+procedure TPagePrincipal.ProgressBarAnimation(aProgressBar: TFMXObject);
+begin
+  with TProgressBar(aProgressBar) do
+    begin
+      Value := 0;
+      while not(Value = Max) do
+        begin
+          Value := Value + 1;
+
+        end;
+    end;
+
 end;
 
 procedure TPagePrincipal.Router;
@@ -69,9 +87,26 @@ begin
 end;
 
 procedure TPagePrincipal.Animation(aLayout: TFMXObject);
+var
+  myThread: TThread;
 begin
+  myThread := TThread.CreateAnonymousThread(
+      procedure
+    begin
+      with ProgressBar do
+        begin
+          Visible := true;
+          while not(Value = Max) do
+            begin
+              Value := TLayout(aLayout).Opacity * 100;
+            end;
+          Value := 0;
+          Visible := false;
+        end;
+    end);
   TLayout(aLayout).Opacity := 0;
   TLayout(aLayout).AnimateFloat('Opacity', 1, 0.7);
+  myThread.Start;
 end;
 
 Initialization

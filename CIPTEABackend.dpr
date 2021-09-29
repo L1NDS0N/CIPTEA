9db0952926a6752626a7f75a8fab0f397a8178bd
Lindson França <l1nds0n@hotmail.com>
2021-09-29 16:01:13 -0300

    new services added

--- CIPTEABackend.dpr
+++ CIPTEABackend.dpr
@@ -5,6 +5,7 @@ program CIPTEABackend;
 
 uses
   Horse,
+  Horse.JWT,
   Horse.ETag,
   Horse.Jhonson,
   Horse.Compression,
@@ -14,12 +15,19 @@ uses
   Controllers.CarteiraPTEA in 'src\Controllers\Controllers.CarteiraPTEA.pas',
   Services.CarteiraPTEA in 'src\Services\Services.CarteiraPTEA.pas' {ServiceCarteiraPTEA: TDataModule},
   Services.Connection in 'src\Services\Services.Connection.pas' {ServiceConnection: TDataModule},
-  Utils.ImageFormat in 'src\Utils\Utils.ImageFormat.pas';
+  Utils.ImageFormat in 'src\Utils\Utils.ImageFormat.pas',
+  Services.User in 'src\Services\Services.User.pas' {ServiceUser: TDataModule},
+  Controllers.Auth in 'src\Controllers\Controllers.Auth.pas',
+  Providers.Authorization in 'src\Providers\Providers.Authorization.pas';
 
 begin
-  THorse.Use(Compression()).Use(OctetStream).Use(Jhonson).Use(ETag).Use(HandleException);
+  ReportMemoryLeaksOnShutdown := True;
+
+  THorse.Use(Compression()).Use(OctetStream).Use(Jhonson).Use(ETag)
+    .Use(HandleException);
 
   Controllers.CarteiraPTEA.Registry;
+  Controllers.Auth.Registry;
 
   THorse.Listen(9000);
 

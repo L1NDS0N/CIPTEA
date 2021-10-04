b5a188c601634471c3d849ffb0023b813298b092
Lindson França <l1nds0n@hotmail.com>
2021-10-04 17:30:58 -0300

    edit user functionality updated

--- src/Pages/Pages.NewUser.pas
+++ src/Pages/Pages.NewUser.pas
@@ -21,27 +21,29 @@ uses
   FMX.Edit,
   FMX.Controls.Presentation,
   FMX.Layouts,
-  Router4D.Interfaces;
+  Router4D.Interfaces,
+  Router4D.Props,
+  Services.User;
 
 type
   TPageNewUser = class(TForm, iRouter4DComponent)
     lytNewUser: TLayout;
     lytInterno: TLayout;
     VertScrollBox: TVertScrollBox;
-    edtSenha: TEdit;
+    edtNovaSenha: TEdit;
     ClearEditButton2: TClearEditButton;
-    edtConfirmarSenha: TEdit;
+    edtConfirmarNovaSenha: TEdit;
     ClearEditButton1: TClearEditButton;
     edtNomeUsuario: TEdit;
     ClearEditButton8: TClearEditButton;
     edtEmail: TEdit;
     ClearEditButton3: TClearEditButton;
-    lblSenha: TLabel;
-    lblConfirmarSenha: TLabel;
+    lblNovaSenha: TLabel;
+    lblConfirmarNovaSenha: TLabel;
     lblNomeUsuario: TLabel;
     lblEmail: TLabel;
     lytHeader: TLayout;
-    Label1: TLabel;
+    lblTitle: TLabel;
     btnVoltar: TRectangle;
     ColorAnimation2: TColorAnimation;
     lblBtnVoltar: TLabel;
@@ -53,14 +55,28 @@ type
     ShadowEffect1: TShadowEffect;
     PasswordEditButton1: TPasswordEditButton;
     PasswordEditButton2: TPasswordEditButton;
+    FloatAnimation4: TFloatAnimation;
+    edtSenhaAtual: TEdit;
+    ClearEditButton4: TClearEditButton;
+    PasswordEditButton3: TPasswordEditButton;
+    lblSenhaAtual: TLabel;
+    cbAlterarSenha: TCheckBox;
+    lytNovasSenhas: TLayout;
+    lytSenhaAtual: TLayout;
     procedure btnVoltarClick(Sender: TObject);
     procedure edtEmailExit(Sender: TObject);
     procedure retBtnSalvarClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure cbAlterarSenhaChange(Sender: TObject);
     private
+      ServiceUser: TServiceUser;
       procedure ValidarCampos;
+      procedure LimparCampos;
     public
       function Render: TFmxObject;
       procedure UnRender;
+      [Subscribe]
+      procedure Props(aValue: TProps);
   end;
 
 var
@@ -71,19 +87,23 @@ implementation
 uses
   Providers.PrivateRoute,
   Utils.Tools,
-  ToastMessage,
-  Services.User;
+  ToastMessage;
 
 {$R *.fmx}
 
 procedure TPageNewUser.btnVoltarClick(Sender: TObject);
 begin
   try
-    OpenPrivateRoute('Dashboard');
+    OpenPrivateRoute('Config');
   except
     on E: Exception do
       TToastMessage.show('Erro durante navegação para a página principal - ' + E.Message, ttDanger);
   end;
+end;
+
+procedure TPageNewUser.cbAlterarSenhaChange(Sender: TObject);
+begin
+  lytNovasSenhas.Visible := cbAlterarSenha.IsChecked;
 end;
 
 procedure TPageNewUser.edtEmailExit(Sender: TObject);
@@ -91,32 +111,72 @@ begin
   ValidarCampos;
 end;
 
+procedure TPageNewUser.FormCreate(Sender: TObject);
+begin
+  lytSenhaAtual.Visible := false;
+end;
+
+procedure TPageNewUser.LimparCampos;
+begin
+  cbAlterarSenha.IsChecked := false;
+  edtNovaSenha.Text := EmptyStr;
+  edtConfirmarNovaSenha.Text := EmptyStr;
+  edtNomeUsuario.Text := EmptyStr;
+  edtEmail.Text := EmptyStr;
+  edtSenhaAtual.Text := EmptyStr;
+  lblTitle.Text := 'Novo usuário';
+  lytSenhaAtual.Visible := false;
+  lytNovasSenhas.Visible := true;
+  lblNovaSenha.Text := 'Senha';
+  lblConfirmarNovaSenha.Text := 'Confirmar Senha';
+end;
+
+procedure TPageNewUser.Props(aValue: TProps);
+begin
+  if (aValue.PropString <> EmptyStr) AND (aValue.Key = 'IdUserToUpdate') then
+    begin
+      lblNovaSenha.Text := 'Nova Senha';
+      lblConfirmarNovaSenha.Text := 'Confirmar nova senha';
+      lytSenhaAtual.Visible := true;
+      lytNovasSenhas.Visible := false;
+
+      ServiceUser.qryUsuarioLocal.Open;
+      lblTitle.Text := 'Editar usuário #' + ServiceUser.qryUsuarioLocalid.AsString;
+      edtNomeUsuario.Text := ServiceUser.qryUsuarioLocalNome.Value;
+      edtEmail.Text := ServiceUser.qryUsuarioLocalEmail.Value;
+    end;
+
+end;
+
 function TPageNewUser.Render: TFmxObject;
 begin
   Result := lytNewUser;
-end;
-
-procedure TPageNewUser.retBtnSalvarClick(Sender: TObject);
-var
-  LService: TServiceUser;
-begin
-  LService := TServiceUser.Create(nil);
-  Self.ValidarCampos;
-  try
-    LService.mtUsuarionome.Value := edtNomeUsuario.Text;
-    LService.mtUsuarioemail.Value := edtEmail.Text;
-    LService.mtUsuariosenha.Value := edtSenha.Text;
-
-    if LService.SalvarNovoUsuário then
-      TToastMessage.show('Novo usuário salvo com sucesso!', ttSuccess);
-
-  finally
-    LService.Free;
-  end;
+  ServiceUser := TServiceUser.Create(nil);
 end;
 
 procedure TPageNewUser.UnRender;
 begin
+  ServiceUser.Free;
+  Self.LimparCampos;
+end;
+
+procedure TPageNewUser.retBtnSalvarClick(Sender: TObject);
+begin
+  Self.ValidarCampos;
+
+  if cbAlterarSenha.IsChecked then
+    begin
+      if not(ServiceUser.EfetuarLogin(ServiceUser.qryUsuarioLocalNome.Value, edtSenhaAtual.Text, true)) then
+        abort;
+      ServiceUser.mtUsuarioid.Value := ServiceUser.qryUsuarioLocalid.Value;
+    end;
+
+  ServiceUser.mtUsuarionome.Value := edtNomeUsuario.Text;
+  ServiceUser.mtUsuarioemail.Value := edtEmail.Text;
+  ServiceUser.mtUsuariosenha.Value := edtNovaSenha.Text;
+
+  if ServiceUser.SalvarNovoUsuario then
+    TToastMessage.show('Novo usuário salvo com sucesso!', ttSuccess);
 
 end;
 
@@ -131,7 +191,7 @@ begin
         end;
     end;
 
-  if edtSenha.Text <> edtConfirmarSenha.Text then
+  if edtNovaSenha.Text <> edtConfirmarNovaSenha.Text then
     begin
       TToastMessage.show('As senhas inseridas não correspondem', ttWarning);
       abort;

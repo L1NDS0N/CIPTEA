65fb5fe1dd2cf0a5e2a414e1a0b146341f0b8639
Lindson França <l1nds0n@hotmail.com>
2021-10-07 15:51:14 -0300

    pagination added to the project

--- CIPTEABackend.dpr
+++ CIPTEABackend.dpr
@@ -1,4 +1,4 @@
-program CIPTEABackend;
+﻿program CIPTEABackend;
 
 {$APPTYPE CONSOLE}
 {$R *.res}
@@ -8,15 +8,16 @@ uses
   Horse.JWT,
   Horse.ETag,
   Horse.Jhonson,
+  Horse.Paginate,
   Horse.Compression,
   Horse.OctetStream,
   Horse.HandleException,
   System.SysUtils,
   Controllers.CarteiraPTEA in 'src\Controllers\Controllers.CarteiraPTEA.pas',
-  Services.CarteiraPTEA in 'src\Services\Services.CarteiraPTEA.pas' {ServiceCarteiraPTEA: TDataModule},
-  Services.Connection in 'src\Services\Services.Connection.pas' {ServiceConnection: TDataModule},
+  Services.CarteiraPTEA in 'src\Services\Services.CarteiraPTEA.pas' {ServiceCarteiraPTEA: TDataModule} ,
+  Services.Connection in 'src\Services\Services.Connection.pas' {ServiceConnection: TDataModule} ,
   Utils.ImageFormat in 'src\Utils\Utils.ImageFormat.pas',
-  Services.User in 'src\Services\Services.User.pas' {ServiceUser: TDataModule},
+  Services.User in 'src\Services\Services.User.pas' {ServiceUser: TDataModule} ,
   Controllers.Auth in 'src\Controllers\Controllers.Auth.pas',
   Providers.Authorization in 'src\Providers\Providers.Authorization.pas',
   Configs.Global in 'src\Config\Configs.Global.pas',
@@ -25,12 +26,18 @@ uses
 begin
   ReportMemoryLeaksOnShutdown := True;
 
-  THorse.Use(Compression()).Use(OctetStream).Use(Jhonson).Use(ETag)
-    .Use(HandleException);
+  THorse.Use(Compression()).Use(OctetStream).Use(Paginate).Use(Jhonson).Use(ETag).Use(HandleException);
 
   Controllers.CarteiraPTEA.Registry;
   Controllers.Auth.Registry;
 
-  THorse.Listen(9000);
+  THorse.Listen(9000,
+      procedure(Horse: THorse)
+    begin
+      Writeln('O Servidor está rodando na porta ' + THorse.Port.ToString);
+      Write('Pressione enter para parar...');
+      ReadLn;
+      THorse.StopListen;
+    end);
 
 end.

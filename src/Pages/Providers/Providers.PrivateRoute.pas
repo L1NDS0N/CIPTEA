c2ccde77866db39634d9aed84e3b1ebbd2798b2f
Lindson França <l1nds0n@hotmail.com>
2021-10-08 17:17:09 -0300

    Paralelism added

--- src/Pages/Providers/Providers.PrivateRoute.pas
+++ src/Pages/Providers/Providers.PrivateRoute.pas
@@ -3,6 +3,7 @@ unit Providers.PrivateRoute;
 interface
 
 uses
+  System.Threading,
   Router4D.Props,
   Configs.GLOBAL;
 
@@ -16,7 +17,8 @@ uses
   Services.Connection,
   Router4D,
   FireDAC.Comp.Client,
-  System.SysUtils;
+  System.SysUtils,
+  System.Classes;
 
 var
   Config: TConfigGLobal;
@@ -33,38 +35,67 @@ var
   LResponse: IResponse;
   LRequest: IRequest;
 begin
-
-  qryConsultaUsuario := TFDQuery.Create(nil);
-  ServiceConnection := TServiceConnection.Create(nil);
-
-  try
-    try
-      qryConsultaUsuario.Connection := ServiceConnection.LocalConnection;
-      qryConsultaUsuario.SQL.Add('select token from usuario limit 1');
-      qryConsultaUsuario.Open;
-
-      if not(qryConsultaUsuario.IsEmpty) then
+  TTask.Run(
+      procedure
+    begin
+      TThread.Synchronize(TThread.Current,
+          procedure
         begin
-          if not(qryConsultaUsuario.FieldByName('token').IsNull) then
-            begin
-              LRequest := TRequest.New.BaseURL(Config.BaseURL).Resource('healthcheck')
-                .TokenBearer(qryConsultaUsuario.FieldByName('token').AsString).RaiseExceptionOn500(true);
+          qryConsultaUsuario := TFDQuery.Create(nil);
+          ServiceConnection := TServiceConnection.Create(nil);
 
-              LResponse := LRequest.Get;
-              if LResponse.StatusCode = 204 then
+          try
+            try
+              qryConsultaUsuario.Connection := ServiceConnection.LocalConnection;
+              qryConsultaUsuario.SQL.Add('select token from usuario limit 1');
+              qryConsultaUsuario.Open;
+
+              if not(qryConsultaUsuario.IsEmpty) then
                 begin
-                  try
-                    if AProps <> nil then
-                      TRouter4D.Link.&To(ARoute, AProps)
-                    else
-                      TRouter4D.Link.&To(ARoute);
-                  except
-                    on E: Exception do
-                      raise Exception.Create('Erro durante navegação para a página #' + ARoute + '. ' + E.Message);
-                  end;
+                  if not(qryConsultaUsuario.FieldByName('token').IsNull) then
+                    begin
+                      LRequest := TRequest.New.BaseURL(Config.BaseURL).Resource('healthcheck')
+                        .TokenBearer(qryConsultaUsuario.FieldByName('token').AsString).RaiseExceptionOn500(true);
 
+                      LResponse := LRequest.Get;
+                      if LResponse.StatusCode = 204 then
+                        begin
+                          try
+                            if AProps <> nil then
+                              TRouter4D.Link.&To(ARoute, AProps)
+                            else
+                              TRouter4D.Link.&To(ARoute);
+                          except
+                            on E: Exception do
+                              raise Exception.Create('Erro durante navegação para a página #' + ARoute + '. ' +
+                                  E.Message);
+                          end;
+
+                        end
+                      else if LResponse.StatusCode = 401 then
+                        begin
+                          try
+                            TRouter4D.Link.&To('Login');
+                          except
+                            on E: Exception do
+                              raise Exception.Create('Erro durante navegação para a página de login. ' + E.Message);
+                          end;
+                          raise Exception.Create
+                            ('Não foi possível validar as suas credenciais de acesso. Suas credenciais expiraram ou são inválidas.');
+                        end;
+                    end
+                  else
+                    begin
+                      try
+                        TRouter4D.Link.&To('Login');
+                      except
+                        on E: Exception do
+                          raise Exception.Create('Erro durante navegação para a página de login. ' + E.Message);
+                      end;
+                      raise Exception.Create('Não foi possível encontrar as suas credenciais de acesso.');
+                    end;
                 end
-              else if LResponse.StatusCode = 401 then
+              else
                 begin
                   try
                     TRouter4D.Link.&To('Login');
@@ -73,39 +104,19 @@ begin
                       raise Exception.Create('Erro durante navegação para a página de login. ' + E.Message);
                   end;
                   raise Exception.Create
-                    ('Não foi possível validar as suas credenciais de acesso. Suas credenciais expiraram ou são inválidas.');
+                    ('Não foi possível encontrar nenhuma credencial de acesso, por favor, efetue login.');
                 end;
-            end
-          else
-            begin
-              try
-                TRouter4D.Link.&To('Login');
-              except
-                on E: Exception do
-                  raise Exception.Create('Erro durante navegação para a página de login. ' + E.Message);
-              end;
-              raise Exception.Create('Não foi possível encontrar as suas credenciais de acesso.');
-            end;
-        end
-      else
-        begin
-          try
-            TRouter4D.Link.&To('Login');
-          except
-            on E: Exception do
-              raise Exception.Create('Erro durante navegação para a página de login. ' + E.Message);
-          end;
-          raise Exception.Create('Não foi possível encontrar nenhuma credencial de acesso, por favor, efetue login.');
-        end;
 
-    except
-      on E: Exception do
-        raise Exception.Create(E.Message);
-    end;
-  finally
-    qryConsultaUsuario.Free;
-    ServiceConnection.Free;
-  end;
+            except
+              on E: Exception do
+                raise Exception.Create(E.Message);
+            end;
+          finally
+            qryConsultaUsuario.Free;
+            ServiceConnection.Free;
+          end;
+        end);
+    end);
 end;
 
 end.

a16d73ef60ad6d3386b39bc97fc9b788c0f5620d
Lindson França <l1nds0n@hotmail.com>
2021-10-14 17:18:47 -0300

    more env vars and functionalities

--- src/Configs/Configs.GLOBAL.pas
+++ src/Configs/Configs.GLOBAL.pas
@@ -8,10 +8,17 @@ type
       function GetExpires: Integer;
       function GetSecret: string;
       function GetBaseURL: string;
+      function GetBasePort: string;
+      function GetBaseHost: string;
+      function GetDBDir: string;
+
     public
       property Expires: Integer read GetExpires;
       property Secret: string read GetSecret;
       property BaseURL: string read GetBaseURL;
+      property BasePort: string read GetBasePort;
+      property BaseHost: string read GetBaseHost;
+      property DBDir: string read GetDBDir;
 
   end;
 
@@ -20,9 +27,24 @@ implementation
 uses
   System.SysUtils;
 
+function TConfigGlobal.GetBaseHost: string;
+begin
+  Result := GetEnvironmentVariable('CIPTEA_HOST');
+end;
+
+function TConfigGlobal.GetBasePort: string;
+begin
+  Result := GetEnvironmentVariable('CIPTEA_PORT');
+end;
+
 function TConfigGlobal.GetBaseURL: string;
 begin
-  Result := GetEnvironmentVariable('CIPTEA_URL');
+  Result := ('http://' + GetEnvironmentVariable('CIPTEA_HOST') + ':' + BasePort);
+end;
+
+function TConfigGlobal.GetDBDir: string;
+begin
+  Result := GetEnvironmentVariable('CIPTEA_DBDIR');
 end;
 
 function TConfigGlobal.GetExpires: Integer;
